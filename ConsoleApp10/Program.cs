7f75618b99f15e972a269506e35d82d616f6c40b
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-09-17 23:57:49 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки A:");
+            Console.Write("X: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты точки B:");
+            Console.Write("X: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            double length = CalculateDistance(x1, y1, x2, y2);
+
+            Console.WriteLine($"Длина отрезка AB: {length}");
+        }
+
+        static double CalculateDistance(double x1, double y1, double x2, double y2)
+        {
+            double deltaX = x2 - x1;
+            double deltaY = y2 - y1;
+            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            return distance;
+        }
+    }
+}
